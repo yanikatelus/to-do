aed2c447ed9a7730e63b7a260c72bc3d5ecfc026
yanikatelus <yanikatelus@gmail.com>
2019-11-09 19:57:59 -0500

    Added ability to delete items on to-do list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public todos = ['Submit Project 3', 'Study Angular docs', 'Review Ionic components'];
 
-  constructor() {}
+  constructor( public alertController: AlertController, private _ngZone: NgZone ) {}
+
+     async presentAddNewPrompt() {
+        console.log('It works so far!');
+        const addTodoAlert = await this.alertController.create(
+            {
+                header: 'Add a Todo',
+                message: 'Enter your todo',
+                inputs: [
+                    {
+                        type: 'text',
+                        name: 'newTodoItem',
+                        placeholder: 'New Item'
+                    }
+                ],
+                buttons: [
+                    {
+                        text: 'Cancel',
+                        role: 'cancel',
+                        handler: () => {
+                            console.log('Confirm Cancel');
+                        }
+                    }, {
+                        text: 'OK',
+                        handler: (inputData) => {
+                            let todo;
+                            if (inputData.newTodoItem) {
+                                todo = inputData.newTodoItem.trim();
+                                if (todo !== '') {
+                                    // Why are we wrapping this into NgZone.run function?
+                                    this._ngZone.run(() => {
+                                        this.todos.push(todo);
+                                    });
+                                } else {
+                                    console.log('The input string is empty.');
+                                }
+                            } else {
+                                console.log('The input string is not set.');
+                            }
+                            return todo;
+                        }
+                    }
+                ]
+            });
+        await addTodoAlert.present();
+    }
+
+    removeTodo(todo: string){
+      console.log("todo = "+todo);
+      let index = this.todos.indexOf(todo);
+      console.log("index of = "+index);
+      this.todos.splice(index,1);
+    }
 
 }
